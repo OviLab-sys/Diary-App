275b41c2b8a4fa80da5914c3b5dc446dcdd611f3
vicoduor <victoroduorr@gmail.com>
2024-04-12 17:11:23 +0300

    .

---
+++ todo/api/models.py
@@ -0,0 +1,50 @@
+from pydantic import BaseModel
+from typing import List,Optional 
+from fastapi import Form
+
+
+class Todo(BaseModel):
+    id: Optional[int]
+    item: str 
+
+    @classmethod
+    def as_form(
+        cls,
+        item: str = Form(...)
+    ):
+        return cls(item=item)
+
+    class Config:
+        Schema_extra = {
+            "Example": {
+                "id":1,
+                "item":"Example schema!"
+            }
+        }
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        schema_extra = {
+            "exampe": {
+                "item":"Read the next chapter of the book"
+            }
+        }
+
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+
+    class Config:
+        schema_extra = {
+            "example":{
+                "todos":[
+                    {
+                        "item": "Example schema 1!"
+                    },
+                    {
+                        "item": "Example schema 2!"
+                    }
+                ]
+            }
+        }
---
+++ todo/api/todo.py
@@ -0,0 +1,75 @@
+from fastapi import APIRouter, Path,HTTPException, status, Request, Depends
+from fastapi.templating import Jinja2Templates
+from models import Todo,TodoItem, TodoItems
+
+todo_router = APIRouter()
+
+
+todo_list = []
+
+templates = Jinja2Templates(directory="templates/")
+
+@todo_router.post("/todo")
+async def add_todo(request: Request, todo: Todo = Depends(Todo.as_form)):
+    todo.id = len(todo_list) + 1
+    todo_list.append(todo)
+    return templates.TemplateResponse("todo/todo.html",{
+        "request":request,
+        "todos":todo_list
+    })
+
+
+@todo_router.get("/todo", response_model=TodoItems)
+async def retrieve_todos(request: Request):
+    return templates.TemplateResponse("/todo/todo.html", {
+        "request":request,
+        "todos": todo_list
+    })
+
+@todo_router.get("/todo/{todo_id}")
+async def todo_detail_view(request: Request,todo_id: int = Path(..., title="The ID of the todo to retrieve.")):
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return templates.TemplateResponse(
+                "todo/todo.html", {
+                    "request":request,
+                    "todo":todo
+                }
+            )
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo with supplied ID doesn't exist",
+    )
+
+@todo_router.put("/todo/{todo_id}")
+async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="The ID of the todo to be updated")) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            todo.item = todo_data.item
+            return {
+                "message":"todo updated successfully."
+            }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo with supplied ID doesn't exist"
+    )
+
+@todo_router.delete("/todo/{todo_id}")
+async def delete_single_todo(todo_id: int) -> dict:
+    for index in range(len(todo_list)):
+        todo = todo_list[index]
+        if todo.id == todo_id:
+            todo_list.pop(index)
+            return {
+                'message': "Todo deleted successfully."
+            }
+    return {
+        "message":"Todo with supplied ID doesn't exist."
+    }
+
+@todo_router.delete("/todo")
+async def delete_all_todo() -> dict:
+    todo_list.clear()
+    return {
+        "message":"Todos deleted successfully."
+    }
